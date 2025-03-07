8564db1c32efde518de886edb4c05fe27f5e31e8
Skordio <skordion@hotmail.com>
2025-03-07 09:26:28 -0500

    setup repo to run electron with ts

--- FrontEndApp/vite.config.ts
+++ FrontEndApp/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig(({ mode }) => ({
+    build: {
+        outDir: './../dist',
+        chunkSizeWarningLimit: 1000,
+    },
+    base: mode == 'development' ? '' : './',
+    plugins: [vue()],
+    server: {
+        port: 3000,
+    },
+}));
